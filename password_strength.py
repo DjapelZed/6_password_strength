cb3c066e70c86a5291bfa862785ca6cd4b95be5b
Maksim Kushnirov <maks.kushnirov@gmail.com>
2017-03-03 19:58:30 +0300

    Реорганизовал код так чтобы вся работа с весовыми коэффициентами была в одном месте

--- password_strength.py
+++ password_strength.py
@@ -1,41 +1,49 @@
 import re
 
-password_strength = {'length': 0}
+
+#WEIGHTS
+PASSWORD_LENGTH_WEIGHT = {'max' : 3, 'medium' : 2, 'min' : 1}
+PASSWORD_CASE_WEIGHT = {'max': 2, 'min' : 0}
+PASSWORD_NUMS_WEIGHT = {'max' : 2, 'medium' : 1, 'min' : 0}
+PASSWORD_CHARS_WEIGTH = {'max' : 2, 'medium' : 1, 'min' : 0}
+PASSWORD_BLACKLIST_WEIGHT = {'max' : 1, 'min' : 0}
 
 
 def get_password_length(password):
     if len(password) >= 10:
-        password_strength['length'] = 3
-        return password_strength['length']
+        return PASSWORD_LENGTH_WEIGHT['max']
     elif len(password) >= 8:
-        return 2
+        return PASSWORD_LENGTH_WEIGHT['medium']
     elif len(password) >= 6:
-        return 1
+        return PASSWORD_LENGTH_WEIGHT['min']
     return 0
 
 
 def get_password_case(password):
     if password != password.lower() and password != password.upper():
-        return 2
-    return 0
+        return PASSWORD_CASE_WEIGHT['max']
+    else:
+        return PASSWORD_CASE_WEIGHT['min']
 
 
 def get_password_nums(password):
     password_nums = re.findall(r'\d', password)
     if len(password_nums) > 4:
-        return 2
+        return PASSWORD_NUMS_WEIGHT['max']
     elif len(password_nums) > 2:
-        return 1
-    return 0
+        return PASSWORD_NUMS_WEIGHT['medium']
+    else:
+        return PASSWORD_NUMS_WEIGHT['min']
 
 
-def get_password_chars(passsword):
-    passsword_chars = len(re.findall(r'\W', passsword))
-    if passsword_chars >= 1:
-        return 2
-    elif passsword_chars > 2:
-        return 1
-    return 0
+def get_password_chars(password):
+    password_chars = len(re.findall(r'\W', password))
+    if password_chars >= 2:
+        return PASSWORD_CHARS_WEIGTH['max']
+    elif password_chars >= 1:
+        return PASSWORD_CHARS_WEIGTH['medium']
+    else:
+        return PASSWORD_CHARS_WEIGTH['min']
 
 
 def load_data(filepath):
@@ -46,8 +54,9 @@ def load_data(filepath):
 
 def compare_with_blacklist(data, password):
     if password not in data:
-        return 1
-    return 0
+        return PASSWORD_BLACKLIST_WEIGHT['max']
+    else:
+        return PASSWORD_BLACKLIST_WEIGHT['min']
 
 
 def get_password_strength(password, filepath):
